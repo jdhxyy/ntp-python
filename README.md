a9f968c83afc81e50210605c9cf411308e995a63
jdhxyy <jdh821@163.com>
2021-04-02 11:44:17 +0800

    Defect:REQ025.003 Modify:zhouxin Desc:增加结构体返回服务

--- README.md
+++ README.md
@@ -11,9 +11,10 @@
 ## 服务
 服务号|服务
 ---|---
-1|读取时间
+1|读取时间1
+2|读取时间2.返回的是结构体
 
-### 读取时间服务
+### 读取时间服务1
 - CON请求：空或者带符号的1个字节。
 
 当CON请求为空时，则默认为读取的是北京时间（时区8）。
@@ -26,10 +27,30 @@
 
 当前时间字符串的格式：2006-01-02 15:04:05 -0700 MST
 
+### 读取时间服务2.返回的是结构体
+- CON请求：格式与读取时间服务1一致
+
+- ACK应答：
+```c
+struct {
+    // 时区
+    uint8 TimeZone
+    uint16 Year
+    uint8 Month
+    uint8 Day
+    uint8 Hour
+    uint8 Minute
+    uint8 Second
+    // 星期
+    uint8 Weekday
+}
+```
+
 ### 自定义错误码
 错误码|含义
 ---|---
-0x40|接收格式错误
+0x40|内部错误
+0x41|接收格式错误
 
 ## 示例
 ### 读取时间
@@ -63,4 +84,49 @@ print("err:", err, "time:", resp)
 输出：
 ```text
 err: 0 time: b'2021-03-21 20:08:14 -0600 MST'
+```
+
+### 读取结构体格式时间
+```python
+import tziot
+import sbc
+
+
+class AckRidGetTime(sbc.LEFormat):
+    _fields_ = [
+        # (字段名, c类型)
+        # 时区
+        ('time_zone', sbc.c_uint8),
+        ('year', sbc.c_int16),
+        ('month', sbc.c_uint8),
+        ('day', sbc.c_uint8),
+        ('hour', sbc.c_uint8),
+        ('minute', sbc.c_uint8),
+        ('second', sbc.c_uint8),
+        # 星期
+        ('weekday', sbc.c_uint8),
+    ]
+
+
+def main():
+    pipe = tziot.bind_pipe_net(0x2141000000000401, pwd, '192.168.1.119', 12021)
+    while not tziot.is_conn():
+        pass
+    resp, err = tziot.call(pipe, 0x2141000000000005, 2, 3000, bytearray())
+    if err != 0:
+        return
+
+    ack = AckRidGetTime()
+    if not ack.bytearray_to_struct(resp):
+        return
+    print(ack.time_zone, ack.year, ack.month, ack.day, ack.hour, ack.minute, ack.second, ack.weekday)
+
+
+if __name__ == '__main__':
+    main()
+```
+
+输出：
+```text
+8 2021 4 2 11 43 7 5
 ```
--- main.py
+++ main.py
@@ -9,17 +9,39 @@ import config
 import tziot
 import dcompy as dcom
 import lagan
+import sbc
 
 from datetime import datetime, timedelta
 
 TAG = 'ntp'
 
 # 应用错误码
+# 内部错误
+ERROR_CODE_INTERNAL_ERROR = 0x40
 # 接收格式错误
-ERROR_CODE_RX_FORMAT = 0x40
+ERROR_CODE_RX_FORMAT = 0x41
 
 # rid号
-RID_GET_TIME = 1
+# 读取时间.返回的是字符串
+RID_GET_TIME1 = 1
+# 读取时间.返回的是结构体
+RID_GET_TIME2 = 2
+
+
+class AckRidGetTime2(sbc.LEFormat):
+    _fields_ = [
+        # (字段名, c类型)
+        # 时区
+        ('time_zone', sbc.c_uint8),
+        ('year', sbc.c_int16),
+        ('month', sbc.c_uint8),
+        ('day', sbc.c_uint8),
+        ('hour', sbc.c_uint8),
+        ('minute', sbc.c_uint8),
+        ('second', sbc.c_uint8),
+        # 星期
+        ('weekday', sbc.c_uint8),
+    ]
 
 
 def main():
@@ -31,10 +53,11 @@ def main():
     dcom.set_filter_level(lagan.LEVEL_WARN)
 
     tziot.bind_pipe_net(config.LOCAL_IA, config.local_pwd, config.LOCAL_IP, config.LOCAL_PORT)
-    tziot.register(RID_GET_TIME, ntp_service)
+    tziot.register(RID_GET_TIME1, ntp_service1)
+    tziot.register(RID_GET_TIME2, ntp_service2)
 
 
-def ntp_service(pipe: int, src_ia: int, req: bytearray) -> (bytearray, int):
+def ntp_service1(pipe: int, src_ia: int, req: bytearray) -> (bytearray, int):
     """校时服务.返回值是应答和错误码.错误码为0表示回调成功,否则是错误码"""
     ip, port = dcom.pipe_to_addr(pipe)
 
@@ -50,13 +73,42 @@ def ntp_service(pipe: int, src_ia: int, req: bytearray) -> (bytearray, int):
 
     now = datetime.utcnow() + timedelta(hours=time_zone)
     if time_zone >= 0:
-        s = b'%04d-%02d-%02d %02d:%02d:%02d +%02d00 MST' % (now.year, now.month, now.day, now.hour, now.minute,
+        s = '%04d-%02d-%02d %02d:%02d:%02d +%02d00 MST' % (now.year, now.month, now.day, now.hour, now.minute,
                                                             now.second, time_zone)
     else:
-        s = b'%04d-%02d-%02d %02d:%02d:%02d -%02d00 MST' % (now.year, now.month, now.day, now.hour, now.minute,
+        s = '%04d-%02d-%02d %02d:%02d:%02d -%02d00 MST' % (now.year, now.month, now.day, now.hour, now.minute,
                                                             now.second, -time_zone)
     lagan.info(TAG, 'ip:%s port:%d ntp time:%s', ip, port, s)
-    return bytearray(s), 0
+    return tziot.str_to_bytearray(s), 0
+
+
+def ntp_service2(pipe: int, src_ia: int, req: bytearray) -> (bytearray, int):
+    """校时服务.返回值是应答和错误码.错误码为0表示回调成功,否则是错误码"""
+    ip, port = dcom.pipe_to_addr(pipe)
+
+    if len(req) == 0:
+        time_zone = 8
+    elif len(req) == 1:
+        time_zone = req[0]
+        if time_zone >= 0x80:
+            time_zone = -(0x100 - time_zone)
+    else:
+        lagan.warn(TAG, "ip:%s port:%d ia:0x%x ntp failed.len is wrong:%d", ip, port, src_ia, len(req))
+        return None, ERROR_CODE_RX_FORMAT
+
+    now = datetime.utcnow() + timedelta(hours=time_zone)
+    t = AckRidGetTime2()
+    t.time_zone = time_zone
+    t.year = now.year
+    t.month = now.month
+    t.day = now.day
+    t.hour = now.hour
+    t.minute = now.minute
+    t.second = now.second
+    t.weekday = now.isoweekday()
+    lagan.info(TAG, 'ip:%s port:%d ntp time:%04d-%02d-%02d %02d:%02d:%02d +%02d00 MST', ip, port, t.year, t.month,
+               t.day, t.hour, t.minute, t.second, t.time_zone)
+    return t.struct_to_bytearray(), 0
 
 
 if __name__ == '__main__':
